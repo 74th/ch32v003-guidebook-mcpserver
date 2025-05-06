5bf27fa343110f0156605f79c03f88ec818750d8
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-05-06 14:21:50 +0900

    up

--- release_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-./generate_documents_blob.py
-mkdir release
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o release/ch32v003-mcp-server_linux_amd64 .
-GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o release/ch32v003-mcp-server_linux_arm64 .
-GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o release/ch32v003-mcp-server_darwin_amd64 .
-GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -o release/ch32v003-mcp-server_darwin_arm64 .
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o release/ch32v003-mcp-server_windows_amd64.exe .
-GOOS=windows GOARCH=arm64 CGO_ENABLED=0 go build -o release/ch32v003-mcp-server_windows_arm64.exe .
